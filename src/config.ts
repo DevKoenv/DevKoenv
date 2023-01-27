db5c0eaf476a514ab26a39c71432e88238eecf35
Koen van Vlimmeren <koenvanvlimmeren@gmail.com>
2023-01-27 12:27:18 +0100

    📝Updated Website Metadata

--- src/config.ts
+++ src/config.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Personal Portfolio | Koenv'; // Site title
-export const SITE_DESCRIPTION = 'Astro Modern Personal Website template with Project Section, CV Section, Paginated Blog, RSS Feed, SEO Friendly, Visual themes and Responsive Desing.';
+export const SITE_TITLE = 'Personal Website | Koenv'; // Site title
+export const SITE_DESCRIPTION = 'Discover the journey of self-discovery and personal growth on my website - an open diary of my experiences and learnings.';
